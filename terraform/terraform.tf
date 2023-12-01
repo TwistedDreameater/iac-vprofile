6ab7f3ffe877b791c4fb3563874753d55295643f
TwistedDreameater <swathyvtr@gmail.com>
2023-12-01 18:05:27 +0530

    new push

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "vprofileactions777"
+    bucket = "vprofile777"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 
   required_version = "~> 1.6.3"
